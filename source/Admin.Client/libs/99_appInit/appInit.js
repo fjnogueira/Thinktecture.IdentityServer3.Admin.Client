969e02654107aad921617bdbc1f8637e9a46cbbd
Manuel Rauber <manuel.rauber@thinktecture.com>
2014-12-22 13:14:02 +0100

    added ui bootstrap

--- source/Admin.Client/libs/99_appInit/appInit.js
+++ source/Admin.Client/libs/99_appInit/appInit.js
@@ -6,5 +6,5 @@ $(function () {
     "use strict";
 
     window.app = window.app || { resolver: {} };
-    app.module = angular.module('ttIdentityAdmin', ['ui.router', 'ui.notify', 'ui.grid', 'pascalprecht.translate', 'toggle-switch', 'xeditable']);
+    app.module = angular.module('ttIdentityAdmin', ['ui.router', 'ui.notify', 'ui.grid', 'ui.bootstrap', 'pascalprecht.translate', 'toggle-switch', 'xeditable']);
 })();
