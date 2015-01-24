4b1f44706608727e6f0d3862061b7327a4d19ffa
Manuel Rauber <manuel.rauber@thinktecture.com>
2015-01-24 10:55:33 +0100

    named lookup lists

--- source/Admin.Client/app/appController.js
+++ source/Admin.Client/app/appController.js
@@ -8,32 +8,32 @@
     function AppController(lookupContainer) {
         // TODO: Should the text values be translated
         lookupContainer.addLookup(lookupContainer.keys.scopeTypes, {
-            0: {enumValue: 0, text: 'Identity'},
-            1: {enumValue: 1, text: 'Resource'}
+            identity: {enumValue: 0, text: 'Identity'},
+            resource: {enumValue: 1, text: 'Resource'}
         });
 
         lookupContainer.addLookup(lookupContainer.keys.flows, {
-            0: {enumValue: 0, text: 'Authorization code'},
-            1: {enumValue: 1, text: 'Implicit'},
-            2: {enumValue: 2, text: 'Hybrid'},
-            3: {enumValue: 3, text: 'Client credentials'},
-            4: {enumValue: 4, text: 'Resource owner'},
-            5: {enumValue: 5, text: 'Custom'}
+            authorizationCode: {enumValue: 0, text: 'Authorization code'},
+            implicit: {enumValue: 1, text: 'Implicit'},
+            hybrid: {enumValue: 2, text: 'Hybrid'},
+            clientCredentials: {enumValue: 3, text: 'Client credentials'},
+            resourceOwner: {enumValue: 4, text: 'Resource owner'},
+            custom: {enumValue: 5, text: 'Custom'}
         });
 
         lookupContainer.addLookup(lookupContainer.keys.accessTokenType, {
-            0: {enumValue: 0, text: 'Jwt'},
-            1: {enumValue: 1, text: 'Reference'}
+            jwt: {enumValue: 0, text: 'Jwt'},
+            reference: {enumValue: 1, text: 'Reference'}
         });
 
         lookupContainer.addLookup(lookupContainer.keys.tokenExpiration, {
-            0: {enumValue: 0, text: 'Sliding'},
-            1: {enumValue: 1, text: 'Absolute'}
+            sliding: {enumValue: 0, text: 'Sliding'},
+            absolute: {enumValue: 1, text: 'Absolute'}
         });
 
         lookupContainer.addLookup(lookupContainer.keys.tokenUsage, {
-            0: {enumValue: 0, text: 'Re use'},
-            1: {enumValue: 1, text: 'One time only'}
+            reUse: {enumValue: 0, text: 'Re use'},
+            oneTimeOnly: {enumValue: 1, text: 'One time only'}
         });
     }
 
