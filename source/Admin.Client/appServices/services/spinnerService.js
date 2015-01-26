ef73e9df3888e014aadbfcead252378ba94e7444
Manuel Rauber <manuel.rauber@thinktecture.com>
2015-01-26 22:09:30 +0100

    Timeout for spinnerService to avoid flickering

--- source/Admin.Client/appServices/services/spinnerService.js
+++ source/Admin.Client/appServices/services/spinnerService.js
@@ -2,17 +2,30 @@
     "use strict";
 
     /**
+     * @param $timeout
      * @param usSpinnerService
      * @constructor
      */
-    function SpinnerService(usSpinnerService) {
+    function SpinnerService($timeout, usSpinnerService) {
         var globalSpinnerKey = 'globalSpinner';
+        var globalSpinnerTimeout;
 
         this.startGlobalSpinner = function () {
-            usSpinnerService.spin(globalSpinnerKey);
+            if (angular.isDefined(globalSpinnerTimeout)) {
+                return;
+            }
+
+            globalSpinnerTimeout = $timeout(function () {
+                usSpinnerService.spin(globalSpinnerKey);
+            }, 500);
         };
 
         this.stopGlobalSpinner = function () {
+            if (angular.isDefined(globalSpinnerTimeout)) {
+                $timeout.cancel(globalSpinnerTimeout);
+                globalSpinnerTimeout = undefined;
+            }
+            
             usSpinnerService.stop(globalSpinnerKey);
         }
     }
